2915dae4667aaebab4363705f66b7491ef40be1f
Quan Nt <quannt210399@gmail.com>
2023-04-18 20:19:05 +0700

    #4 Handle event

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,23 @@ import MyComponent from "./components/MyComponent";
 
 function App() {
 
-    const [name, setName] = useState("Quan Nt")
+    const [name, setName] = useState("")
+    const [infor, setInfor] = useState("")
+
+    const handleChange = (event) => {
+        setName(event.target.value)
+    }
+    
+    const handleClick = () => {
+        setInfor(name)
+    }
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <input type="text" value={name}/>
-                <p>Hello Vite + React! My name is {name}</p>
+                <input type="text" value={name} onChange={(event)=> handleChange(event)}/>
+                <button onClick={() => handleClick()}>Submit</button>
+                <p>Hello Vite + React with {infor}</p>
                 <MyComponent/>
             </header>
         </div>
