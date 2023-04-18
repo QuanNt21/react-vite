4f09fcfcdcb27b82da069a836afac64318f48152
Quan Nt <quannt210399@gmail.com>
2023-04-18 19:53:18 +0700

    #3 State

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import MyComponent from "./components/MyComponent";
 
 function App() {
+
+    const [name, setName] = useState("Quan Nt")
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello Vite + React!</p>
+                <input type="text" value={name}/>
+                <p>Hello Vite + React! My name is {name}</p>
                 <MyComponent/>
             </header>
         </div>
